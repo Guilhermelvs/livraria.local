0bb8ae3422dabb8592f7d089fe6702a4bc295fb4
Thiago Soares <thiagosb.moura@gmail.com>
2017-06-02 18:00:01 -0300

    Arquivos de configuração Guilherme

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 // URLs Fast
-define("URL_BASE", "http://livraria.local/");
+define("URL_BASE", "http://localhost/livraria.local/");
 define("URL_CSS", URL_BASE . "assets/css/");
 define("URL_JS", URL_BASE . "assets/js/");
 define("URL_IMG", URL_BASE . "assets/images/");
@@ -10,8 +10,8 @@ define("URL_VIEW", "");
 // Banco de dados Config
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "vagrant");
-define("DB_NAME", "livraria_local");
+define("DB_PASS", "");
+define("DB_NAME", "livraria");
 
 // Page Config Constants
 define("HEAD_TITLE", "Livrarias OTA");
